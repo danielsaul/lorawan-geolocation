bba47a698f2c112c01ae2b72114dfb2b2a563eac
Daniel Saul <daniel@dansaul.co.uk>
2017-03-08 12:16:55 +0000

    Export data from redis script

---
+++ server/thingserver/export_data.py
@@ -0,0 +1,34 @@
+"""
+    ThingServer
+    Export data from redis
+"""
+
+from redis import StrictRedis
+from redis.exceptions import ConnectionError
+
+import settings as s
+
+import json
+
+
+
+if __name__ == '__main__':
+ 
+    r = StrictRedis(host=s.REDIS_HOST, port=s.REDIS_PORT, db=s.REDIS_DB)
+    try:
+        if r.ping():
+            print "Redis connected."
+    except ConnectionError:
+        "Error: Redis server not available."
+
+    keys = r.lrange('packet_list', 0, -1)
+
+    data = []
+
+    for key in keys:
+       value = r.get('packet_%d' % key)
+       data.append(json.loads(value))
+
+    with open('packets.json', 'w') as f:
+        json.dump(data, f)
+        
